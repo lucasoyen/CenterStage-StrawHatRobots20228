69801b90fe886edea658ad7a5784e24bbe2ce90e
loyen24 <lucasoyen920@gmail.com>
2024-02-06 12:41:26 -0500

    penis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tanky.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tanky.java
@@ -5,29 +5,26 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
 import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior;
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.SampleTankDrive;
 
 @TeleOp(
         group = "drive"
 )
-public class newdrive extends LinearOpMode {
+public class tanky extends LinearOpMode {
     private double speed = 1.0;
     private boolean speedcanbechanged = true;
 
-    public newdrive() {
-    }
-
     public void runOpMode() throws InterruptedException {
-        SampleMecanumDrive drive = new SampleMecanumDrive(this.hardwareMap);
+        SampleTankDrive drive = new SampleTankDrive(hardwareMap);
         drive.setMode(RunMode.RUN_USING_ENCODER);
         drive.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
-        this.waitForStart();
+        waitForStart();
 
         while(opModeIsActive()){
             drive.setWeightedDrivePower((new Pose2d(
                     (double)this.gamepad1.left_stick_y,
                     (double)this.gamepad1.left_stick_x,
-                            (double)(this.gamepad1.right_bumper?1:0)-(this.gamepad1.left_bumper?1:0)))
+                    (double)(this.gamepad1.right_bumper?1:0)-(this.gamepad1.left_bumper?1:0)))
                     .times(this.speed*0.8));
             if (this.gamepad1.x && this.speedcanbechanged) {
                 this.speedcanbechanged = false;
